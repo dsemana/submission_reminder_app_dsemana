0cd8a59261d3fdcb5b80300feb29fc436f7c8d9d
dsemana <d.semana@alustudent.com>
2025-10-17 03:56:15 -0700

    new

--- copilot_shell_script.sh
+++ copilot_shell_script.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-echo 'Assignment name: '
-read assignmentName
 
-echo $assignmentName
+# Ask for new assignment name
+read -p "Enter the new assignment name: " NEW_ASSIGNMENT
+
+# Update ASSIGNMENT value in config/config.env
+sed -i "s/^ASSIGNMENT=.*/ASSIGNMENT=\"${NEW_ASSIGNMENT}\"/" ./config/config.env
+
+echo "Assignment updated to: ${NEW_ASSIGNMENT}"
+echo "Re-running the reminder application..."
+sleep 1
+bash startup.sh
+
